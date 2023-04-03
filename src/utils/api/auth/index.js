8b2238edc803eb6422ec4a5e4cb3cea680fa59da
bashayer770 <beshow95@gmail.com>
2023-04-03 15:22:43 +0300

    query chatting

--- src/utils/api/auth/index.js
+++ src/utils/api/auth/index.js
@@ -1,22 +1,31 @@
 import { configureAuth } from "react-query-auth";
+import instance from "../../api";
 import axios from "../../api";
+import { storage } from "../../storage";
 
 const { useUser, useLogin, useRegister, useLogout, AuthLoader } = configureAuth(
   {
     userFn: () => {
       // YOUR FUNCTION THAT GETS THE USER GOES HERE
     },
-    loginFn: (credentials) => {
+    loginFn: async (credentials) => {
       // YOU FUNCTION FOR LOGIN HERE
-      axios.post("/signin", credentials);
+      let login = await instance.post("/signin", credentials);
+      const token1 = login.data.token1;
+      storage.setToken(token1);
     },
-    registerFn: (credentials) => {
+    registerFn: async (credentials) => {
       // YOUR FUNCTION FOR SIGNUP HERE
-      axios.post("/signup", credentials);
+      let res = await instance.post("/signup", credentials);
+      const token = res.data.token;
+      storage.setToken(token);
     },
     logoutFn: () => {
       // YOUR FUNCTION FOR LOGOUT HERE
+      storage.clearToken();
     },
   }
 );
 export { useUser, useLogin, useRegister, useLogout, AuthLoader };
+
+//const res= usequery(["list"], ()=> ({data : "test"}));
--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,8 +1,24 @@
 import axios from "axios";
+import { storage } from "../storage";
 
 const instance = axios.create({
   // Add your backend url here
   baseURL: "https://task-react-axios-be.herokuapp.com",
 });
+
+instance.interceptors.request.use(
+  function (config) {
+    //Do something before request is sent
+    const token = storage.getToken();
+    if (token) {
+      config.headers.authorization = `bearer ${token}`;
+    }
+    return config;
+  },
+  function (error) {
+    //Do something with request error
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
